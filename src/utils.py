3d3372d48f1debf4a5efc3deca9a8016ed0938ab
Ciror3 <crussi@udesa.esc.ar>
2024-12-09 10:58:02 -0300

    Error en el prerpocesamiento

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def stratified_split(route,X,y,y_bins,name,test=True):
+def stratified_split(route,X,y,y_bins,name,test):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.15, random_state=42, stratify=y_bins
     )
     train_data = pd.concat([X_train, y_train], axis=1)
     test_data = pd.concat([X_test, y_test], axis=1)
     train_data.to_csv(f'{route}/{name}_train.csv')
-    if test is None:
+    if test is True:
         test_data.to_csv(f'{route}/{name}_test.csv')
     else:
         test_data.to_csv(f'{route}/{name}_val.csv')
